7a5796d99e9eb741ab6db9e00e5826b3773a290b
Simon F <github@19r.de>
2017-03-03 17:47:51 +0100

    v2.0.1α
    Versionsnummer wurde überschrieben.

--- scripts/server.ps1
+++ scripts/server.ps1
@@ -3,7 +3,7 @@
 ### Startbildschirm ###
     function startbildschirm {
         Write-Host "╔═══════════════════════════════════════════════════════════════════════════════╗"
-        Write-Host "║ Windows Product Key Tool v2.0.1α                                              ║"
+        Write-Host "║ Windows Product Key Tool v2.0.1α                                              ║" 
         Write-Host "║                                                                               ║"
         Write-Host "╚═══════════════════════════════════════════════════════════════════════════════╝"
     }
